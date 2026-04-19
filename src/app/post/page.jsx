f3be070a96a38e619d965d1025eba19f2fcf4ea0
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-04-19 08:00:32 +0600

    part 3

--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -1,11 +1,37 @@
+// // const getPosts=async()=>{
+// // const res=await fetch('https://jsonplaceholder.typicode.com/posts')
+// // return res.json()
+// // }
+// const posts=await getPosts()
+
+
 // const getPosts=async()=>{
-// const res=await fetch('https://jsonplaceholder.typicode.com/posts')
+// try{
+//     const res=await fetch('https://jsonplaceholder.typicode.com/posts')
 // return res.json()
 // }
+// catch(err){
+//     throw new Error ('failed to fetch posts')
+    
+// }
+
+// }
+
+ const getPosts=async()=>{
+const res=await fetch('https://jsonplaceholder.typicode.com/posts')
+if(!res.ok){
+    throw new Error ('Failed to fetch posts')
+}
+return res.json()
+}
 const posts=await getPosts()
+
+
+
 const PostsPage =async() => {
-    const res= await fetch('https://jsonplaceholder.typicode.com/posts')
-    const posts=await res.json()
+    // const res= await fetch('https://jsonplaceholder.typicode.com/posts')
+    // const posts=await res.json()
+    // const posts=getPosts()
     return (
         <div>
            <h2>Total Posts: {posts.length}</h2>
